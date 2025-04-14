c14d301d0084181ff0a176bbbd3af3dde38232c9
Sadamori Kojaku <skojaku@iu.edu>
2025-04-14 12:27:39 -0400

    add assignment

--- grading-toolkit/grade_notebook.sh
+++ grading-toolkit/grade_notebook.sh
@@ -16,13 +16,13 @@ ungraded_notebook="tmp_ungraded.ipynb"
 ungraded_py="tmp_ungraded.py"
 
 # Get the cells with tag "gradable"
-jq '.cells |= map(select(.metadata.tags and (.metadata.tags | index("'$cell_tag'"))))' $assignment_notebook > $ungraded_notebook
+uv run jq '.cells |= map(select(.metadata.tags and (.metadata.tags | index("'$cell_tag'"))))' $assignment_notebook > $ungraded_notebook
 
 #jupyter nbconvert --to notebook --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='["non-grading-item"]' --output $ungraded_notebook $ungraded_notebook
 
-jupytext $ungraded_notebook --to py
+uv run jupytext $ungraded_notebook --to py
 
 cat $ungraded_py $test_python_script > tmp_run.py
-uv python tmp_run.py || { echo "Assertion error occurred"; exit 1; }
+uv run python tmp_run.py || { echo "Assertion error occurred"; exit 1; }
 rm tmp_run.py
 rm $ungraded_notebook $ungraded_py
