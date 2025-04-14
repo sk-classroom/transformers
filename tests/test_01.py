044fec9b1027475e5bef39ed92e1497da1fe9efc
Sadamori Kojaku <skojaku@iu.edu>
2025-04-14 12:22:53 -0400

    add assignment

--- tests/test_01.py
+++ tests/test_01.py
@@ -1,13 +1,95 @@
 # %% Import
-from scipy import sparse
 import pandas as pd
-import igraph
 import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import torch
+from transformers import AutoTokenizer, AutoModel
 
-# %% Define utility functions
+def load_data(focal_word, is_train, n_samples=100):
+    data_type = "train" if is_train else "test"
+    data_file = f"https://raw.githubusercontent.com/danlou/bert-disambiguation/master/data/CoarseWSD-20/{focal_word}/{data_type}.data.txt"
+    label_file = f"https://raw.githubusercontent.com/danlou/bert-disambiguation/master/data/CoarseWSD-20/{focal_word}/{data_type}.gold.txt"
 
-def to_igraph(src, trg):
-    g = igraph.Graph.TupleList(list(zip(src, trg)), directed=False)
-    return g
+    data_table = pd.read_csv(
+        data_file,
+        sep="\t",
+        header=None,
+        dtype={"word_pos": int, "sentence": str},
+        names=["word_pos", "sentence"],
+    )
+    label_table = pd.read_csv(
+        label_file,
+        sep="\t",
+        header=None,
+        dtype={"label": int},
+        names=["label"],
+    )
+    combined_table = pd.concat([data_table, label_table], axis=1)
+    return combined_table.sample(n_samples)
+
+def train_classifier(emb, labels):
+    """
+    Train a classifier on the embeddings.
+    """
+    clf = LinearDiscriminantAnalysis(n_components=1)
+    clf.fit(emb, labels)
+    return clf
+
+def evaluate_classifier(clf, emb, labels):
+    """
+    Evaluate the performance of the classifier.
+    """
+    prediction = clf.predict(emb)
+    accuracy = np.mean(prediction == labels)
+    return accuracy
+
 
 # %% Test -----------
+
+def load_data(focal_word, is_train, n_samples=100):
+    data_type = "train" if is_train else "test"
+    data_file = f"https://raw.githubusercontent.com/danlou/bert-disambiguation/master/data/CoarseWSD-20/{focal_word}/{data_type}.data.txt"
+    label_file = f"https://raw.githubusercontent.com/danlou/bert-disambiguation/master/data/CoarseWSD-20/{focal_word}/{data_type}.gold.txt"
+
+    data_table = pd.read_csv(
+        data_file,
+        sep="\t",
+        header=None,
+        dtype={"word_pos": int, "sentence": str},
+        names=["word_pos", "sentence"],
+    )
+    label_table = pd.read_csv(
+        label_file,
+        sep="\t",
+        header=None,
+        dtype={"label": int},
+        names=["label"],
+    )
+    combined_table = pd.concat([data_table, label_table], axis=1)
+    return combined_table.sample(n_samples)
+
+focal_word = "java"
+train_data = load_data(focal_word, is_train=True)
+test_data = load_data(focal_word, is_train=False)
+
+# Get the embeddings of the test set
+emb_train = get_token_embedding(train_data["sentence"].values, train_data["word_pos"].values)
+
+# Get the embeddings of the test set
+emb_test = get_token_embedding(test_data["sentence"].values, test_data["word_pos"].values)
+
+# Train the classifier
+clf = train_classifier(emb_train, train_data["label"].values)
+
+# Get the predictions
+prediction = clf.predict(emb_test)
+
+# Evaluate the performance
+accuracy = evaluate_classifier(clf, emb_test, test_data["label"].values)
+
+
+assert accuracy > 0.91
